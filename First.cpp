8989c66d00b443527d7ffd4616f41ad2b6ab5c46
Ajithkumar <ajith30bsc@gmail.com>
2025-05-22 23:37:57 +0530

    FindMiddleNumber

--- First.cpp
+++ First.cpp
@@ -3,31 +3,19 @@
 using namespace std;
 int main() {
    
-    vector<int> v ={11,34,1,23,45,98,12};
+    vector<int> v ={11,34,1,23,45,98,12,34};
+    int slowcount=0;
+    int fastcount=0;
+    cout << v.size() << endl;
+   while (fastcount < v.size() && fastcount+1 < v.size()) {
+    cout << "fastcount: " << fastcount << endl;
+       fastcount += 2;
+       slowcount++;
+    }
+    cout << " " << v[slowcount] << endl;
 
-    for ( int i = 0; i < v.size(); i++)
-    {
-        int currentindex=i;
-        for (int j = i; j < v.size(); j++)
-        {
-            if(v[j] > v[currentindex])
-            {
-                currentindex=j;
-            }
-            /* code */
-        }
-        int temp = v[i];
-        v[i] = v[currentindex];
-        v[currentindex] = temp;
-        /* code */
-    }
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << endl;
-        /* code */
-    }
-    
-    
-    cout << "Hello, World!" << endl;
+  
+  
+    //cout << "Hello, World!" << endl;
     return 0;
 }
