80b7bd691ace68fb9d6afdf9a487875be8664c5f
Ajithkumar <ajith30bsc@gmail.com>
2025-05-24 20:25:13 +0530

    findmiddlenumber

--- First.cpp
+++ First.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 int main() {
    
-    vector<int> v ={11,34,1,23,45,98,2,34};
-    int slowcount=0;
-    int fastcount=0;
-    cout << v.size() << endl;
-   while (fastcount < v.size() && fastcount+1 < v.size()) {
-    cout << "fastcount: " << fastcount << endl;
-       fastcount += 2;
-       slowcount++;
-       if(v[slowcount] == v[fastcount]){
-         
-        cout<<"Loop detected at: " << v[slowcount] << endl;
-        return 0;
-       }
-    }
-    cout << " " << v[slowcount] << endl;
+    vector<int> v ={23,11,34,1,23,98,2,34};
+   int slow=0;
+   int fast=0;
+
+   while (fast<v.size() && fast+1<v.size())
+   {
+
+    slow++;
+    fast+2;
+
+    /* code */
+   }
+   cout<< slow<<endl;
 
   
-  
-    //cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
